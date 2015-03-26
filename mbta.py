eecb20507133a0af07a7cc2e4ea9e10c3e1cf1e3
unknown <colangelod@DC-Surface.wit.private>
2015-03-26 18:04:52 -0400

    7

---
+++ mbta.py
@@ -0,0 +1,82 @@
+from pprint import pprint
+import json
+import urllib2
+import time
+
+from multiprocessing.pool import ThreadPool as Pool
+
+pool_size = 35
+pool = Pool(pool_size)
+
+
+URL="http://realtime.mbta.com/developer/api/v2/{0}?api_key=3yCIHX5F2ketSKY0T6s0LA&format=json"
+
+ROUTE_TYPES = ['0', '1']
+
+CMD_ROUTES = "routes"
+CMD_SCHEDULE_BY_ROOT = "schedulebyroute"
+CMD_PREDICTIONS = "predictionsbytrip"
+CMD_ALERTS = "alertsbyroute"
+
+res = urllib2.urlopen(URL.format(CMD_ROUTES))
+
+js_res = json.load(res)
+
+routes = []
+trip_res = []
+
+def main():
+    for mode in js_res['mode']:
+        if mode['route_type'] in ROUTE_TYPES:
+            for route in mode['route']:
+                route_id = route['route_id']
+                print route_id
+                res = urllib2.urlopen(URL.format(CMD_SCHEDULE_BY_ROOT)+"&route={0}&datetime={1}".format(route['route_id'], int(time.time())))
+                schd = json.load(res)
+                alert_res = False
+                try:
+                    res = urllib2.urlopen(URL.format(CMD_ALERTS)+"&route={0}".format(route['route_id']))
+                    alert_res = json.load(res)
+                except urllib2.HTTPError:
+                    pass
+                except AttributeError:
+                    pass
+                temp = {}
+                temp.update({
+                    "route_id": route['route_id'],
+                    "route_title": route['route_name'],
+                    "mode_name": mode['mode_name'],
+                    "directions": schd['direction'],
+                    "alerts": alert_res,
+                    "predictions": []
+                })
+                for direction in schd['direction']:
+                    for trip in direction['trip']:
+                        #pool.apply_async(get_directions, (trip, route_id,))
+                        try:
+                            res = urllib2.urlopen(URL.format(CMD_PREDICTIONS)+"&trip={0}".format(trip['trip_id']))
+                            temp['predictions'].append(json.load(res))
+                        except urllib2.HTTPError:
+                            pass
+                        except AttributeError:
+                            pass
+                        except Exception as e:
+                            print(e)
+
+                routes.append(temp)
+
+
+    pool.close()
+    pool.join()
+    with open("mbta.json", "w") as out:
+        #pprint(routes, stream=out)
+        json.dump(routes, out, indent=4, sort_keys=True)
+
+
+def get_directions(trip, route_id):
+    pass
+
+if __name__ == "__main__":
+    t0 = time.time()
+    main()
+    print time.time() - t0
---
+++ mbta_mysql.py
@@ -0,0 +1,100 @@
+#port MySQLdb
+import json
+import MySQLdb as mdb
+from pprint import pprint
+
+cnx = mdb.connect('localhost', 'root', '', 'databaseproject')
+cursor = cnx.cursor()
+
+file = open("mbta.json", "r")
+
+json_data = json.load(file)
+
+for train in json_data:
+    route_id = train['route_id']
+    route_title = train['route_title']
+    mode_name = train['mode_name']
+    directions = train['directions']
+    alerts = train['alerts']
+    predictions = train['predictions']
+    cursor.execute( "select RouteID from TrainRoutes where RouteID = (%s)", [route_id])
+    res = cursor.fetchone()
+    if res is None:
+        cursor.execute( "insert into TrainRoutes (RouteID, RouteName, ModeName) VALUES (%s,%s,%s)", (route_id, route_title, mode_name))
+    #print route_id, route_title
+
+    for direction in directions:
+        direction_id = direction['direction_id']
+        direction_name = direction['direction_name']
+        trips = direction['trip']
+
+
+        #print direction_id, direction_name
+
+
+
+    for prediction in predictions:
+        already_trip = False
+        #pprint(prediction)
+        direction_name = prediction['direction_name']
+        route_id = prediction['route_id']
+        route_type = prediction['route_type']
+        trip_id = prediction['trip_id']
+        mode_name = prediction['mode_name']
+        trip_name = prediction['trip_name']
+        trip_headsign = prediction['trip_headsign']
+
+        if "vehicle" in prediction:
+            vehicle_id = prediction['vehicle']['vehicle_id']
+            vehicle_lat = prediction['vehicle']['vehicle_lat']
+            vehicle_lon = prediction['vehicle']['vehicle_lon']
+            vehicle_bearing = prediction['vehicle']['vehicle_bearing']
+            vehicle_timestamp = prediction['vehicle']['vehicle_timestamp']
+
+            if not already_trip:
+                cursor.execute( "select TripID from TrainTrips where TripID = (%s)", [trip_id])
+                res = cursor.fetchone()
+                if res is None:
+                    cursor.execute( "insert into TrainTrips (TripID, RouteId, VehicleId, TripHeadsign) VALUES (%s,%s,%s,%s)", (trip_id, route_id, vehicle_id, trip_headsign ))  #  % vars()
+                already_trip = True
+            cursor.execute( "select VehicleID from TrainLocations where VehicleID = (%s) and TripID = (%s)", (vehicle_id, trip_id))
+            res = cursor.fetchone()
+            if res is None:
+                cursor.execute( "insert into TrainLocations (TrainLAT, TrainLon,TripID,RouteId,VehicleID) VALUES (%s,%s,%s,%s,%s)", (vehicle_lat,vehicle_lon,trip_id,route_id,vehicle_id) )#  % vars()
+            else:
+                cursor.execute( "update TrainLocations set TrainLAT = (%s), TrainLON = (%s) where TripID = (%s) and VehicleID = (%s)", (vehicle_lat,vehicle_lon,trip_id,vehicle_id) )
+            stop_ids = []
+            for stop in prediction['stop']:
+                pre_away = stop['pre_away']
+                stop_sequence = stop['stop_sequence']
+                stop_name = stop['stop_name']
+                stop_id = stop['stop_id']
+                if stop_id not in stop_ids:
+                    stop_ids.append(stop_id)
+                    cursor.execute( "select TripID from TripStops where TripID = (%s) and StopID = (%s)", (trip_id, stop_id))
+                    res = cursor.fetchone()
+                    if res is None:
+                        cursor.execute( "insert into TripStops (TripID, StopID, StopName, StopSequence, PredAway) VALUES (%s,%s,%s,%s,%s)",(trip_id,stop_id,stop_name,stop_sequence,pre_away))  # % vars()
+                    else:
+                        cursor.execute( "update TripStops set StopID = (%s), StopName = (%s), StopSequence = (%s), PredAway = (%s) where TripID = (%s)", (stop_id,stop_name,stop_sequence,pre_away, trip_id) )
+    route_id = alerts['route_id']
+    route_name = alerts['route_name']
+    for alert in alerts['alerts']:
+        alert_id = alert['alert_id']
+        alert_text = alert['header_text']
+        cursor.execute( "select AlertID from Alerts where RouteID = (%s) and AlertID = (%s)", (route_id, alert_id))
+        res = cursor.fetchone()
+        if res is None:
+            cursor.execute( "insert into Alerts (RouteID, AlertID, AlertText) values (%s,%s,%s)", (route_id, alert_id, alert_text))
+    print "\n\n"
+
+    route_id = alerts['route_id']
+    route_name = alerts['route_name']
+    for alert in alerts['alerts']:
+        alert_id = alert['alert_id']
+        alert_text = alert['description_text']
+        print "INSERT INTO Alerts " \
+              "(RouteID, AlertID, AlertText) " \
+              "VALUES ('%(route_id)s', '%(alert_id)s', '%(alert_text)s');" % vars()
+
+cnx.commit()
